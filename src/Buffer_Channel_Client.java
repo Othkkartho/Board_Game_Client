1dcf2a80f17b8de6466cab10243854900fb83297
qhtkdrma <kimwonkyu15@gmail.com>
2022-12-03 21:14:07 +0900

    개발 중

--- src/Buffer_Channel_Client.java
+++ src/Buffer_Channel_Client.java
@@ -2,26 +2,43 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.nio.channels.SocketChannel;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Buffer_Channel_Client {
     public Buffer_Channel_Client() {
         SocketAddress address = new InetSocketAddress("127.0.0.1", 5001);
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
         try (SocketChannel channel = SocketChannel.open(address)) {
             System.out.println("Connected to Chat Server");
-            String message;
-            Scanner scanner = new Scanner(System.in);
+            System.out.print("Name: ");
+            String name = scanner.nextLine();
+            HelperMethods.sendMessage(channel, name);
+
+//            System.out.println("Waiting for Other Player ...");
+//            System.out.println("Message: " + HelperMethods.receiveMessage(channel));
+
+            System.out.println("주사위를 굴리려면 y, 끝내려면 n을 입력해 주세요");
             while (true) {
-                System.out.println("Waiting for message from the server ...");
-                System.out.println("Message: " + HelperMethods.receiveMessage(channel));
-                System.out.print("> ");
-                message = scanner.nextLine();
-                if (message.equalsIgnoreCase("quit")) {
-                    HelperMethods.sendMessage(channel, "Client terminating");
+                String game = scanner.nextLine();
+
+                if (game.equals("Y") || game.equals("y")) {
+                    int diceNum = random.nextInt(1, 7);
+                    System.out.println("\n주사위 숫자는 " + diceNum + "입니다.");
+                    HelperMethods.sendMessage(channel, String.valueOf(diceNum));
+                }
+                else if (game.equals("N") || game.equals("n")) {
+                    HelperMethods.sendMessage(channel, String.valueOf(0));
                     break;
                 }
-                HelperMethods.sendMessage(channel, message);;
+                else {
+                    System.out.println("주사위를 굴리려면 y, 끝내려면 n을 입력해 주세요");
+                }
+
+                System.out.println("Waiting for Other Player ...");
+                System.out.println("Message: " + HelperMethods.receiveMessage(channel));
             }
         } catch (IOException e) {
             e.printStackTrace();
--- src/HelperMethods.java
+++ src/HelperMethods.java
@@ -3,36 +3,6 @@ import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 
 public class HelperMethods {
-
-    public static void sendFixedLengthMessage(SocketChannel socketChannel, String message) {
-        try {
-            ByteBuffer buffer = ByteBuffer.allocate(64);
-            buffer.put(message.getBytes());
-            buffer.flip();
-            while (buffer.hasRemaining()) {
-                socketChannel.write(buffer);
-            }
-            System.out.println("Sent: " + message);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    public static String receiveFixedLengthMessage(SocketChannel socketChannel) {
-        String message = "";
-        try {
-            ByteBuffer byteBuffer = ByteBuffer.allocate(64);
-            socketChannel.read(byteBuffer);
-            byteBuffer.flip();
-            while (byteBuffer.hasRemaining()) {
-                message += (char) byteBuffer.get();
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-        return message;
-    }
-
     public static void sendMessage(SocketChannel socketChannel, String message) {
         try {
             ByteBuffer buffer = ByteBuffer.allocate(message.length() + 1);
@@ -42,7 +12,6 @@ public class HelperMethods {
             while (buffer.hasRemaining()) {
                 socketChannel.write(buffer);
             }
-            System.out.println("Sent: " + message);
         } catch (IOException ex) {
             ex.printStackTrace();
         }
@@ -51,7 +20,7 @@ public class HelperMethods {
     public static String receiveMessage(SocketChannel socketChannel) {
         try {
             ByteBuffer byteBuffer = ByteBuffer.allocate(16);
-            String message = "";
+            StringBuilder message = new StringBuilder();
             while (socketChannel.read(byteBuffer) > 0) {
                 char byteRead = 0x00;
                 byteBuffer.flip();
@@ -60,14 +29,14 @@ public class HelperMethods {
                     if (byteRead == 0x00) {
                         break;
                     }
-                    message += byteRead;
+                    message.append(byteRead);
                 }
                 if (byteRead == 0x00) {
                     break;
                 }
                 byteBuffer.clear();
             }
-            return message;
+            return message.toString();
         } catch (IOException ex) {
             ex.printStackTrace();
         }
