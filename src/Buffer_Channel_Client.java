87d319eddcf2fbb1b1986de0e7f14acfa65f3800
qhtkdrma <kimwonkyu15@gmail.com>
2022-12-13 16:31:50 +0900

    개발 중

--- src/Buffer_Channel_Client.java
+++ src/Buffer_Channel_Client.java
@@ -1,24 +1,26 @@
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Buffer_Channel_Client {
     public Buffer_Channel_Client() {
-        SocketAddress address = new InetSocketAddress("127.0.0.1", 5001);
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
+        String msg;
 
-        try (SocketChannel channel = SocketChannel.open(address)) {
+        try {
+            SocketChannel channel = SocketChannel.open(new InetSocketAddress("localhost", 5001));
+
             System.out.println("Connected to Chat Server");
             System.out.print("Name: ");
             String name = scanner.nextLine();
-            HelperMethods.sendMessage(channel, name);
-
-//            System.out.println("Waiting for Other Player ...");
-//            System.out.println("Message: " + HelperMethods.receiveMessage(channel));
+            msg = "name#"+name;
+            HelperMethods.sendMessage(channel, msg);
+            HelperMethods.receiveMessage(channel);
 
             System.out.println("주사위를 굴리려면 y, 끝내려면 n을 입력해 주세요");
             while (true) {
@@ -27,7 +29,12 @@ public class Buffer_Channel_Client {
                 if (game.equals("Y") || game.equals("y")) {
                     int diceNum = random.nextInt(1, 7);
                     System.out.println("\n주사위 숫자는 " + diceNum + "입니다.");
-                    HelperMethods.sendMessage(channel, String.valueOf(diceNum));
+                    msg = name + "#" + String.valueOf(diceNum);
+                    HelperMethods.sendMessage(channel, msg);
+                    msg = HelperMethods.receiveMessage(channel);
+                    System.out.println(msg);
+                    msg = HelperMethods.receiveMessage(channel);
+                    System.out.println(msg);
                 }
                 else if (game.equals("N") || game.equals("n")) {
                     HelperMethods.sendMessage(channel, String.valueOf(0));
@@ -36,9 +43,6 @@ public class Buffer_Channel_Client {
                 else {
                     System.out.println("주사위를 굴리려면 y, 끝내려면 n을 입력해 주세요");
                 }
-
-                System.out.println("Waiting for Other Player ...");
-                System.out.println("Message: " + HelperMethods.receiveMessage(channel));
             }
         } catch (IOException e) {
             e.printStackTrace();
--- src/HelperMethods.java
+++ src/HelperMethods.java
@@ -5,7 +5,7 @@ import java.nio.channels.SocketChannel;
 public class HelperMethods {
     public static void sendMessage(SocketChannel socketChannel, String message) {
         try {
-            ByteBuffer buffer = ByteBuffer.allocate(message.length() + 1);
+            ByteBuffer buffer = ByteBuffer.allocate(message.length()+1);
             buffer.put(message.getBytes());
             buffer.put((byte) 0x00);
             buffer.flip();
@@ -19,8 +19,8 @@ public class HelperMethods {
 
     public static String receiveMessage(SocketChannel socketChannel) {
         try {
-            ByteBuffer byteBuffer = ByteBuffer.allocate(16);
-            StringBuilder message = new StringBuilder();
+            ByteBuffer byteBuffer = ByteBuffer.allocate(64);
+            String message = "";
             while (socketChannel.read(byteBuffer) > 0) {
                 char byteRead = 0x00;
                 byteBuffer.flip();
@@ -29,7 +29,7 @@ public class HelperMethods {
                     if (byteRead == 0x00) {
                         break;
                     }
-                    message.append(byteRead);
+                    message += byteRead;
                 }
                 if (byteRead == 0x00) {
                     break;
@@ -42,5 +42,4 @@ public class HelperMethods {
         }
         return "";
     }
-
 }
